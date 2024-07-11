e757c9eea113c1e25fcc9205f023c378733e5b4c
Aditya <aditya001tomar@gmail.com>
2024-07-11 10:56:06 +0530

    handled the error to save model

--- MTL-E/checkpoint_model.py
+++ MTL-E/checkpoint_model.py
@@ -1,16 +1,24 @@
 import transformers
 import torch
-
+import os
 
 def save_model(model_name, multitask_model):
-    print("hello I AM HERE-----------------------------------------")
     tokenizer = transformers.AutoTokenizer.from_pretrained(model_name)
     for task_name in ["bias", "stereotype"]:
-        multitask_model.taskmodels_dict[task_name].config.to_json_file(
-            f"./{task_name}_model/config.json"
-        )
+        # Create the directory if it doesn't exist
+        save_dir = f"./{task_name}_model"
+        os.makedirs(save_dir, exist_ok=True)
+
+        # Check if config.json does not exist and save the configuration file
+        config_path = os.path.join(save_dir, "config.json")
+        if not os.path.exists(config_path):
+            multitask_model.taskmodels_dict[task_name].config.to_json_file(config_path)
+
+        # Save the model state dict
         torch.save(
             multitask_model.taskmodels_dict[task_name].state_dict(),
-            f"./{task_name}_model/pytorch_model.bin",
+            os.path.join(save_dir, "pytorch_model.bin")
         )
-        tokenizer.save_pretrained(f"./{task_name}_model/", safe_serialization=False)
+
+        # Save the tokenizer
+        tokenizer.save_pretrained(save_dir)
